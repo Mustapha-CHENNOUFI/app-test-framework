56b25381a8997a1fa4a59721ecfd4f221630a494
Mustapha_CHENNOUFI <mohamedmustaphachennoufi@gmail.com>
2023-01-07 22:18:48 +0100

    add some test steps

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('test spec', () => {
+  it('signin', () => {
+    const credential= {
+      usename: 'staphano',
+      password: 'stephano26'
+    };
+    cy.visit('http://localhost:3000/signin')
+    cy.get('#username').type(credential.usename)
+    cy.get('#password').type(credential.password)
+    cy.get("button[type='submit']").click()
+
   })
 })
