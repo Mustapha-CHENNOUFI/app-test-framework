ceb6b6fc2d5605cd0f201ac70edf64c29a58254a
Mustapha_CHENNOUFI <mohamedmustaphachennoufi@gmail.com>
2023-01-08 02:43:59 +0100

    last for today

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,35 @@
-describe('test spec', () => {
-  it('signin', () => {
-    const credential= {
-      usename: 'staphano',
-      password: 'stephano2600'
+describe('User Sign-up and Login', () => {
+  it('Redirect unauthenticated user to signin page', () => {
+    cy.visit('/personal');
+    cy.location('href').then( (url) => { cy.log(url) });
+    cy.location('pathname').should('equal','/signin');
+
+  })
+
+  it('Sign-up', () => {
+    const userInfo= {
+      firstName: "Mustapha",
+      lastName: "Chennoufi",
+      username: "Stoupha1996",
+      password: "s3cret",
     };
-    cy.visit('/signin')
-    cy.get('#username').type(credential.usename)
-    cy.get('#password').type(credential.password)
-    cy.get("button[type='submit']").click()
+    cy.visit('/signup');
+    cy.get('h1[data-test="signup-title"]').should("be.visible")
+    .and("contain", "Sign Up");
+    cy.get('#firstName').type(userInfo.firstName);
+    cy.get('#lastName').type(userInfo.lastName);
+    cy.get('#username').type(userInfo.username);
+    cy.get('#password').type(userInfo.password);
+    cy.get('#confirmPassword').type(userInfo.password);
+    cy.get('.MuiButton-label').should('be.visible').click();
+    cy.intercept('POST', 'http://localhost:3001/signup').as('user');
+    cy.log(user);
+
+    cy.get('#username').type(userInfo.username);
+    cy.get('#password').type(userInfo.password);
+    cy.get("button[type='submit']").should('be.visible').click();
+    //cy.get("div[role='dialog']").screenshot('onboarding-dialog');
+    cy.get('[data-test="user-onboarding-next"]').click();
 
   })
 })
